5b06f6ba74b0f83701f29c7dc75d90c23ce7a20d
Davyd Bredykhin <correodemimobil@gmail.com>
2022-12-24 12:26:27 +0100

    fix

--- validations.js
+++ validations.js
@@ -1,11 +1,11 @@
 import { body } from 'express-validator';
 
-export const loginValidator = [
+export const loginValidation = [
   body('email', 'Wrong format of email').isEmail(),
   body('password', 'The password must contain at least 5 characters').isLength({min: 5}),
 ];
 
-export const registerValidator = [
+export const registerValidation = [
   body('email', 'Wrong format of email').isEmail(),
   body('password', 'The password must contain at least 5 characters').isLength({min: 5}),
   body('fullName', 'The username must contain at least 3 characters').isLength({min: 3}),
